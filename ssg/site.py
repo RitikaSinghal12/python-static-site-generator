7f563ba35b3c9a2692b4806d8ee2c5a093de2143
RitikaSinghal12 <52667707+RitikaSinghal12@users.noreply.github.com>
2021-05-13 14:11:23 -0400

    small miss

--- ssg/site.py
+++ ssg/site.py
@@ -14,4 +14,4 @@ class Site:
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if Path.is_dir(path):
-                Site.create_dir(path)
+                self.create_dir(path)
